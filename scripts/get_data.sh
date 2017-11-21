b1b5fad53711f3b0cacbb3d9465056fd1238922e
Kevin Yu <tianheyu927@gmail.com>
2017-11-21 11:11:41 -0800

    added get_data

---
+++ scripts/get_data.sh
@@ -0,0 +1,4 @@
+wget http://rail.eecs.berkeley.edu/datasets/mil_data.zip
+unzip mil_data.zip
+rm mil_data.zip
+mv textures ~/gym/gym/envs/mujoco/assets/sim_push_xmls/
--- scripts/run_sim_vision_reach.sh
+++ scripts/run_sim_vision_reach.sh
@@ -1,5 +1,5 @@
 # Run the simulated visual reach experiment with default hyperparameters
-python main.py --experiment=sim_vision_reach --demo_file=data/sim_vision_reach_noisy \
+python main.py --experiment=sim_vision_reach --demo_file=data/sim_vision_reach \
                   --demo_gif_dir=data/sim_vision_reach_noisy/ --gif_prefix=color --im_width=80 \
                   --im_height=64 --training_set_size=750 --val_set_size=150 --metatrain_iterations=30000 \
                   --meta_batch_size=25 --train_update_lr=0.001 --clip=True --clip_min=-20 --clip_max=20 --conv_bt=False\
