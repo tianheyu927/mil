192be0f2918f71bba97a3a220d678327f80d75a9
Kevin Yu <tianheyu927@gmail.com>
2017-11-28 22:26:17 -0800

    add comments for getting data

--- scripts/get_data.sh
+++ scripts/get_data.sh
@@ -1,4 +1,5 @@
 wget http://rail.eecs.berkeley.edu/datasets/mil_data.zip
 unzip mil_data.zip
 rm mil_data.zip
+# Change the following to be your path to the fork of the gym repo
 mv textures ~/gym/gym/envs/mujoco/assets/sim_push_xmls/
